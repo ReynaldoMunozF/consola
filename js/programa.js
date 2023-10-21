68ddc27536a821bc303d2b4bf27c73e69d2b9777
ReynaldoMunozF <reynaldo.munozf21@gmail.com>
2023-10-22 00:56:01 +0200

    funcionabilidad de cambio de pantalla

--- js/programa.js
+++ js/programa.js
@@ -1,33 +1,40 @@
 
 
+
+
+
+
+
+
 let puntero= document.getElementById("puntero");
-let velocidad= 10;
+let velocidad= 40;
 let movtop = 0;
 let movleft = 0;
-
+let pantallaJuego = document.getElementById("pantalla-juego");
+let btnB = document.getElementById("boton-dos");
+let btnA = document.getElementById("boton-uno");
+let texto = document.getElementById("texto");
 document.addEventListener("keydown", function(e){
-    console.log("paso por aqui")
    // console.log(e.keycode)
     console.log(e)
     if (e.keyCode == "38"){
         moverArriba();
     }
 })
-function moverArriba(){
-    // console.log("me muevo")
-    // console.log(movtop)
-    // console.log(velocidad)
-    // console.log(puntero)
-
+function moverArriba(){    
+    movtop -=velocidad;
+    puntero.style.marginTop = movtop +"px"
+};
+function moverAbajo(){  
     movtop +=velocidad;
     puntero.style.marginTop = movtop +"px"
-}
-function moverArriba(){
-    // console.log("me muevo")
-    // console.log(movtop)
-    // console.log(velocidad)
-    // console.log(puntero)
+};
 
-    movtop +=velocidad;
-    puntero.style.marginTop = movtop +"px"
-}
+btnB.onclick = function(){
+    
+    pantallaJuego.src = "https://art.pixilart.com/sr2d5bde751f5aws3.gif";
+    texto.textContent = "1 PLAYER (A) 2 PLAYER (B)";
+    
+};
+
+
