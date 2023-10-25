fef32c52f73c9a67b49178ce4398b60bc42a8446
ReynaldoMunozF <reynaldo.munozf21@gmail.com>
2023-10-26 00:07:19 +0200

    cambios en consola por responsive

--- js/programa.js
+++ js/programa.js
@@ -1,7 +1,5 @@
 
-let velocidad = 40;
-let movtop = 0;
-let movleft = 0;
+
 let pantallaJuego = document.getElementById("pantalla-juego");
 let btnB = document.getElementById("boton-dos");
 let btnA = document.getElementById("boton-uno");
@@ -20,84 +18,69 @@ let texto2 = document.getElementById("texto2");
 let adelantar = 0;
 let retroceder = 0;
 
-
-// document.addEventListener("keydown", function (e) {
-//  console.log(e.keycode)
-//      console.log(e)
-//      if (e.keyCode == "38") {
-//         moverArriba();
-//     }
-//  })
-//  function moverArriba() {
-//      movtop -= velocidad;
-//      puntero.style.marginTop = movtop + "px";
-//  };
-//  function moverAbajo() {
-//      movtop += velocidad;
-//      puntero.style.marginTop = movtop + "px";
-//  };
-
-btnEncendido.onclick = function () {  //enciende la consola
-    audio1.play();
-    //pantallaJuego.src = "../img/img1.png";
-    pantallaJuego.style.display = "block";
-    Led.style.backgroundColor = "red";
-
-}
-
-btnApagado.onclick = function () { //apaga la consola
-    audio2.load();
-    pantallaJuego.src = "../img/img0.png";
-    texto.textContent = "";
-    Led.style.backgroundColor = "black";
-    window.location.reload();
-
-
-}
-btnSubirVolumen.onclick = function () { // sube el volumen
-
-    audio2.volume += .1;
-}
-btnBajarVolumen.onclick = function () { // baja el volumen
-
-    audio2.volume -= .1;
-
-}
-
-btnStart.onclick = function () {  //arranca el juego
-    console.log("estoy aqui")
-    audio2.play();
-    pantallaJuego.src = "../img/img1.png";
-    texto.textContent =""
-    texto2.style.display ="block"
-    Led.style.backgroundColor = "red";
-
-}
-
-btnA.onclick = function () {
-    sonidoBtn1.play();
-    texto2.style.display ="none"
-    if( adelantar == 0){
-        pantallaJuego.src = "../img/img3.png";
-        //texto.textContent = "1 PLAYER (A) 2 PLAYER (B)";
-        adelantar=1;
-        retroceder=1;
-    }else if(adelantar == 1){
-        pantallaJuego.src = "../img/img2.png";
-        adelantar=2
-        retroceder =2
-    }else{
-        pantallaJuego.src = "../img/img6.png";
-        adelantar =1 ;
         
-    }
-    
-};
+        btnEncendido.onclick = function () {  //enciende la consola
+            audio1.play();
+            //pantallaJuego.src = "../img/img1.png";
+            pantallaJuego.style.display = "block";
+            Led.style.backgroundColor = "red";
+            
+        }
+
+        btnApagado.onclick = function () { //apaga la consola
+            audio2.load();
+            pantallaJuego.src = "../img/img0.png";
+            texto.textContent = "";
+            Led.style.backgroundColor = "black";
+            window.location.reload();
+            
+            
+        }
+        btnSubirVolumen.onclick = function () { // sube el volumen
+            
+            audio2.volume += .1;
+        }
+        btnBajarVolumen.onclick = function () { // baja el volumen
+            
+            audio2.volume -= .1;
+            
+        }
+        
+        btnStart.onclick = function () {  //arranca el juego
+            console.log("estoy aqui")
+            audio2.play();
+            pantallaJuego.src = "../img/img1.png";
+            texto.textContent = ""
+            texto2.style.display = "block"
+            Led.style.backgroundColor = "red";
+            
+        }
+        
+       
+        btnA.onclick = function () {
+            sonidoBtn1.play();
+            texto2.style.display = "none"
+            if (adelantar == 0) {
+                pantallaJuego.src = "../img/img3.png";
+                //texto.textContent = "1 PLAYER (A) 2 PLAYER (B)";
+                adelantar = 1;
+                retroceder = 1;
+            } else if (adelantar == 1) {
+                pantallaJuego.src = "../img/img2.png";
+                adelantar = 2
+                retroceder = 2
+            } else {
+                pantallaJuego.src = "../img/img6.png";
+                adelantar = 1;
+                
+            }
+            
+        };
 btnB.onclick = function () {
     sonidoBtn1.play();
     pantallaJuego.src = "../img/img3.png";
     
-   
+    
 }
 
 
